34284127c8f690a322e4a0b9787aba582a630789
Luan Carletto <carlettoluan@gmail.com>
2022-08-04 08:53:02 -0300

    lambda 4

---
+++ src/exercicios/lambda/Fornecedor.java
@@ -0,0 +1,16 @@
+package exercicios.lambda;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Supplier;
+
+public class Fornecedor {
+	
+	public static void main(String[] args) {
+		
+		Supplier<List<String>> umLista = 
+				() -> Arrays.asList("Ana", "Bia", "Mia", "Lia");
+				
+				System.out.println(umLista.get());
+	}
+}
